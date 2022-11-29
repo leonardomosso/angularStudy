77e204cb1186f37cc541eef85a56ac6f72df0626
unknown <leonardo_mcv@hotmail.com>
2022-11-28 21:30:37 -0300

    First deploy to the angular studies

---
+++ src/app/mock-taks.ts
@@ -0,0 +1,25 @@
+import { Task } from "./Tasks"
+
+
+export const TASKS: Task[] = [
+    {
+        id: 1,
+        text: "Doctor Appointment",
+        day: "May 5th at 02:30pm",
+        reminder: true,
+    },
+
+    {
+        id: 2,
+        text: "Meeting at School",
+        day: "May 10th at 14:30pm",
+        reminder: true,
+    },
+
+    {
+        id: 3,
+        text: "Food Shopping",
+        day: "May 07th at 12:30pm",
+        reminder: false,
+    }
+]
